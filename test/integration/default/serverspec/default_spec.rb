77746c922dcfbfe1b3b0612291a3954efc073f50
Bill ONeill <woneill@pobox.com>
2018-02-09 18:25:33 -0500

    Refactor kitchen tests to work with current OS versions

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'serverspec'
 
 ## We'll assume a system-level install for testing purposes
 describe file('/usr/local/rbenv') do
@@ -17,10 +17,10 @@ end
 
 describe command('bash -l -c "which rbenv"') do
   its(:stdout) { should match /\/usr\/local\/rbenv\/bin\/rbenv/ }
-  it { should return_exit_status 0 }
+  its(:exit_status) { should eq 0 }
 end
 
 describe command('bash -l -c "rbenv --version"') do
-  its(:stdout) { should match /0\.4\.0/ }
-  it { should return_exit_status 0 }
+  its(:stdout) { should match /^rbenv .*/ }
+  its(:exit_status) { should eq 0 }
 end
