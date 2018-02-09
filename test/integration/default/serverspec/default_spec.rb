48bf88a8b2a7c8534ce5fda06f56a882a289ab2c
Bill ONeill <woneill@pobox.com>
2018-02-09 18:26:24 -0500

    Add support for default-gems with system rbenv installations

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -15,6 +15,20 @@ describe file('/usr/local/rbenv/bin/rbenv') do
   it { should be_grouped_into 'root' }
 end
 
+# Test that default gems were installed
+describe file('/usr/local/rbenv/shims/bundle') do
+  it { should be_file }
+  it { should be_executable }
+  it { should be_executable.by('others') }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+end
+
+describe command('bash -l -c "bundle --version"') do
+  its(:stdout) { should match /^Bundler version .*/ }
+  its(:exit_status) { should eq 0 }
+end
+
 describe command('bash -l -c "which rbenv"') do
   its(:stdout) { should match /\/usr\/local\/rbenv\/bin\/rbenv/ }
   its(:exit_status) { should eq 0 }
